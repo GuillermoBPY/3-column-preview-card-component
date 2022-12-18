4eebf33a2bfb038fcaacb9a357c16db69e73baaa
GuillermoBPY <guillermo-benitez@outlook.com>
2022-12-18 18:55:36 -0300

    Formato

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 let buttsedan = document.querySelector(".btn__sedans");
-buttsedan.addEventListener("click",saludte)
+buttsedan.addEventListener("click", saludte);
 let buttsuv = document.querySelector(".btn__suvs");
-buttsuv.addEventListener("click",saludte)
+buttsuv.addEventListener("click", saludte);
 let buttluxury = document.querySelector(".btn__luxury");
-buttluxury.addEventListener("click",saludte)
+buttluxury.addEventListener("click", saludte);
 
-function saludte (){
-  window.alert("Este Botón Funciona!")
+function saludte() {
+  window.alert("Este Botón Funciona!");
 }
