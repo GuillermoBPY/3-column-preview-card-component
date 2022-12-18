6f3fa2c5aa18132f21377bc0258ae8967067dc13
GuillermoBPY <guillermo-benitez@outlook.com>
2022-12-18 18:54:59 -0300

    Final

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-let butt = document.querySelector
+let buttsedan = document.querySelector(".btn__sedans");
+buttsedan.addEventListener("click",saludte)
+let buttsuv = document.querySelector(".btn__suvs");
+buttsuv.addEventListener("click",saludte)
+let buttluxury = document.querySelector(".btn__luxury");
+buttluxury.addEventListener("click",saludte)
+
+function saludte (){
+  window.alert("Este Botón Funciona!")
+}
